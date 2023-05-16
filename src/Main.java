2335b2cac0cc5e22699a6b48483c81d8ae862c1d
MatheusDuraesDias <matheusdias5158x@gmail.com>
2023-05-16 16:59:46 -0300

    vambora

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Primeira Versão");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Informe seu perfil");
+        sc.nextLine();
     }
 }
